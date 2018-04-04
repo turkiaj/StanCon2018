475fa23d2ef14d5772df27b5dbfbb1c7008fb341
Turkia <jari.turkia@cgi.com>
2018-04-04 09:56:37 +0300

    requirements

---
+++ install_packages.r
@@ -0,0 +1,7 @@
+if (!require(knitr)) install.packages("knitr")
+if (!require(rstan)) install.packages("rstan")
+if (!require(xlsx)) install.packages("xlsx")
+if (!require(igraph)) install.packages("igraph")
+if (!require(rgexf)) install.packages("rgexf")
+if (!require(bayesplot)) install.packages("bayesplot")
+if (!require(ggplot2)) install.packages("ggplot2")
